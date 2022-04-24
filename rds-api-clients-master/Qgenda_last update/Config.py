872e840fc24b8c11caca95ab2e329e5ba067accf
truss120 <truss120@gmail.com>
2022-04-24 17:41:05 -0400

    Delete rds-api-clients-master/Qgenda_last update directory

--- rds-api-clients-master/Qgenda_last update/Config.py
+++
@@ -1,9 +0,0 @@
-
-
-Filelocation = 'C:\\Qgenda\\rds-api-clients\\Qgenda\\Qgenda_Depts.xlsx'
-
-ScheduleLocation = 'C:\\Qgenda\\Archives\\Schedules\\'
-
-RequestLocation =  'C:\\Qgenda\\Archives\\Requests\\'
-
-StaffMemberLocation = 'C:\\Qgenda\\Archives\\StaffMember\\'
