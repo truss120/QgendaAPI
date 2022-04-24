77de861034667ff0bf581a13dd1d453849aef66f
truss120 <truss120@gmail.com>
2022-04-24 17:36:12 -0400

    Delete rds-api-clients-master/CmsAPIClient directory

--- rds-api-clients-master/CmsAPIClient/src/main/java/edu/bcm/api/cms/entity/CmsDataTypes.java
+++
@@ -1,7 +0,0 @@
-package edu.bcm.api.cms.entity;
-
-public class CmsDataTypes {
-    public static final String RESEARCH = "RESEARCH";
-    public static final String OWNERSHIP = "OWNERSHIP";
-    public static final String GENERAL = "GENERAL";
-}
